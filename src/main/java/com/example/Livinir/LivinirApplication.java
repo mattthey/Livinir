6a189ad7b7bb77463d130484afa95a3e3f845939
cookie027 <n.shvedov27@gmail.com>
2019-10-19 22:29:14 +0500

    init app

---
+++ src/main/java/com/example/Livinir/LivinirApplication.java
@@ -0,0 +1,13 @@
+package com.example.Livinir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivinirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivinirApplication.class, args);
+	}
+
+}
