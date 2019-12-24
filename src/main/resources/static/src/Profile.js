129daebee03d24dc791fa3575f39f0ce78472e28
lifare <43409162+lifare@users.noreply.github.com>
2019-12-24 10:33:51 +0500

    createProfile

--- src/main/resources/static/src/Profile.js
+++ src/main/resources/static/src/Profile.js
@@ -22,6 +22,30 @@ class Profile extends Component{
         return(<div>
                 <Menu></Menu>
                 <div className='Profile'>
+                    <div className='DataProfile'>
+                        <h3>Data_user</h3>
+                        <p>wqeqwsfeww: dsewdwe</p>
+                        <p>wqeqwsfeww: wwrqewae</p>
+                        <p>wqeqwsfeww: qwewqeewq</p>
+                        <p>wqeqwsfeww: wqeqweawE</p>
+                    </div>
+                    <form className='DataAd'>
+                        <h3>Create_Ad</h3>
+                        <input type='email' name='email' placeholder='Enter your email' defaultValue='test@gmail.com'/>
+                        <input type='text' name='login' placeholder='Enter your login' defaultValue='user'/>
+                        <input type='text' name='firstName' placeholder='Enter your First Name' defaultValue='user'/>
+                        <input type='text' name='lastName' placeholder='Enter your Last Name' defaultValue='user'/>
+                        <input type='password' name='password' placeholder='Enter password' defaultValue='123456'/>
+                        <input type='date' name='dateOfBirth' placeholder='Enter your birthday' defaultValue='2019-01-19'/>
+                        <br/>
+                        <textarea name="content" placeholder='Enter your additional'></textarea>
+                        <br/>
+                        <p> Your gender:
+                            <input type="radio" name="Sex" value='male' defaultChecked={true}/> Male
+                            <input type="radio" name="Sex" value='female'/> Female
+                        </p>
+                        <button>Create</button>
+                    </form>
                 </div>
             </div>
         )
